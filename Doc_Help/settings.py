569ee5c97e03c97089a7db801e90820baeca9acf
Advitya <advityasharma21@gmail.com>
2020-03-28 00:24:00 +0530

    using postgres sql

--- Doc_Help/settings.py
+++ Doc_Help/settings.py
@@ -81,11 +81,12 @@ WSGI_APPLICATION = 'Doc_Help.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'password':'',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'advitya',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
