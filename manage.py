1ad982fd15af346e6e5f6a7fef59b63cc027ef31
Advitya <advityasharma21@gmail.com>
2020-03-27 16:59:04 +0530

    fixing

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Doc-Help.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Doc_Help.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- register/admin.py
+++ register/admin.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> 65ac7b5834b1d58425ec286c723914a690ca97d9
